6b2499be197b348ac835874bb3d5dddc74ae84ce
7VaGe <nicholas.ciarafoni@studio.unibo.it>
2022-02-23 21:32:23 +0100

    BugFix Script cookies
    Correzioni dell'errore che era presente dentro lo script per i cookie, ora viene eseguito bene, non capisco perché il
    window.cb_hideCookieBanner = hideCookieBanner; non vuole le () che mi segnala un altro errore.

--- script/cookies.js
+++ script/cookies.js
@@ -1,4 +1,6 @@
-$(document).ready( function() {
+$(document).ready( function( ){
+
+
 
 /* Visualiizza il banner dei cookie */
 function showCookieBanner(){
@@ -6,30 +8,22 @@ function showCookieBanner(){
     cookieBanner.style.display = "block";
    }
    
-   /* Nascondo il banner dei cookie e salvo in memoria locale la scelta */
-function hideCookieBanner(){
-    localStorage.setItem("cb_isCookieAccepted", "yes");
+   /* Nascondo il banner dei cookie e salvo in sessiostorage la scelta */
+   function hideCookieBanner(){
+    sessionStorage.setItem("cb_isCookieAccepted", "yes");
     let cookieBanner = document.getElementById("cb-cookie-banner");
     cookieBanner.style.display = "none";
    }
    
-   /* Controllo la memoria locale e mostro il banner */
+  
    function initializeCookieBanner(){
-    let isCookieAccepted = localStorage.getItem("cb_isCookieAccepted");
-    if(isCookieAccepted === null)
+    let isCookieAccepted = sessionStorage.setItem("cb_isCookieAccepted","no");
+    if((isCookieAccepted === null) || (isCookieAccepted === "no"))
     {
-     localStorage.setItem("cb_isCookieAccepted", "no");
      showCookieBanner();
     }
-    if(isCookieAccepted === "no"){
-     showCookieBanner();
-    }
-   }
-   
+}
+
    window.onload = initializeCookieBanner();
    window.cb_hideCookieBanner = hideCookieBanner;
-
-
-
-});
-
+   });
--- template/base.php
+++ template/base.php
@@ -16,14 +16,13 @@
     </head>
     
         <body>
-        
-<div id="cb-cookie-banner" class=" alert alert-warning fade  show  text-center mb-0" role="alert" >
+          
+<div id="cb-cookie-banner" class=" alert alert-warning fade show  text-center mb-0" role="alert" >
           🍪  Questo sito web utilizza i cookie per assicurarti di ottenere la migliore esperienza sul nostro sito web. <br>
           <a href="https://www.garanteprivacy.it/faq/cookie" target="blank">Ulteriori informazioni</a>
-          <button type="button" class="btn btn-primary btn-sm ms-3" onclick="window.cb_hideCookieAlert()"> Accetto! </button>
-
+          <button type="button" class="btn btn-primary btn-sm ms-3" onclick="window.cb_hideCookieBanner()"> Accetto! </button>
          <div class=" d-flex justify-content-end">
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <button type="button" class="btn-close" data-bs-dismiss="alert"  aria-label="Close"></button>
         
          </div>
            
