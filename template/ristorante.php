e2c5b838d97c576d52a044ee7c4cde3305392d01
Nicholas Ciarafoni <78082910+7VaGe@users.noreply.github.com>
2022-03-28 18:08:49 +0200

    NewFeatures Cards
    Cambiata la struttura delle card, devo finire di lavorarci per ottimizzare il template e poi riportare il Main in ristorante.php.

--- template/ristorante.php
+++ template/ristorante.php
@@ -3,20 +3,30 @@
   <?php foreach($templateParams["catego"] as $category):?>
     <li class="mt-3 bg-dark rounded-3 shadow-lg text-white list-group-item d-flex justify-content-center align-items-start">
       <div class="ms-2 me-auto" data-bs-toggle="collapse" data-bs-target="#<?php echo $category["nomecategoria"]?>" aria-expanded="false" aria-controls="collapseWidthExample">
-        <div class="fw-bold"><?php echo $category["nomecategoria"]?></div>
-       <p><?php echo $category["nomecategoria"]?></p>
+        <div class="fw-bold display-1"><?php echo $category["nomecategoria"]?></div>
+        <p><?php echo $category["nomecategoria"]?></p>
       </div>
-      <span class="badge"><i class='mt-3 fas fa-hamburger fa-bounce' style='font-size:48px;color:rgb(247,193,68)'></i></span>
+      <span class="badge"><i class='mt-3 fas fa-hamburger fa-bounce'  style='font-size:48px;color:rgb(247,193,68)'></i></span>
     </li>
   <?php endforeach; ?>
   </ol>
 </div>
-
 <?php foreach($templateParams["catego"] as $category):
   $parametro =$dbh->getProdottoByCategoria($category["idcategoria"]);
    foreach($parametro as $prodotto):?>
-    <div class="col-12 col-sm-4 text-white px-4 py-5 collapse collapse-horizontal" id="<?php echo $category["nomecategoria"]?>" >
-      <div class="row row-cols-1 align-items-center g-4 py-5">
+    <div class="mt-2 row row-cols-1 row-cols-md-2 g-4 collapse collapse-horizontal" id="<?= $category["nomecategoria"]?>">
+    <div class="col d-inline-block">
+      <div class="card bg-dark mt-2 text-white">
+        <img src="<?php echo $prodotto["img"]?>" class="card-img-top" alt="alt immagine">
+        <div class="card-body">
+          <h5 class="card-title display-1"><?php echo $prodotto["nome"]?></h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+    </div>
+   <!-- <div class="col-sm-4 text-white px-4 py-5 collapse collapse-horizontal" id="" >
+      <div class="align-items-center g-4 py-5">
         <div class="col">
           <div class="card card-cover overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="width:250px">
             <div class="d-flex flex-column text-center pb-3 text-white">
@@ -31,6 +41,6 @@
           </div>
         </div>
       </div>
-    </div>
+    </div>-->
   <?php endforeach; ?>
 <?php endforeach; ?>
