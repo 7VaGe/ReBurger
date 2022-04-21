374f5bd22613a0ffcaee64ff27d77aa74b246b62
Nicholas Ciarafoni <78082910+7VaGe@users.noreply.github.com>
2022-04-21 16:52:08 +0200

    BugFixed prodotti.php
    spostare il contenuto di prova che è all'interno di prodotti.php in ristorante.php
    
    Nel db ho inserito altre icone, utili per le nostre categorie, manca quella relativa ai fritti, che è a pagamento.
    o si cambia la categoria e mettiamo altro oppure non saprei come inserirla in modo dinamico con il php.

--- template/prodotti.php
+++ template/prodotti.php
@@ -1,46 +1,5 @@
 
-<!-- 
 
-<div class="container-lg">
-  <ol class="list-group">
-  <?php/* foreach($templateParams["catego"] as $category):?>
-    <li class="mt-3 bg-dark rounded-3 shadow text-white list-group-item d-flex justify-content-center align-items-start">
-      <div class="ms-2 me-auto" data-bs-toggle="collapse" data-bs-target="#<?php echo $category["nomecategoria"]?>" aria-expanded="false" aria-controls="collapseWidthExample">
-        <div class="fw-bold display-1"><?php echo $category["nomecategoria"]?></div>
-      </div>
-      <span class="badge"><i class='<?php echo $category["icona"] ?>' style='font-size:48px;color:rgb(247,193,68)'></i></span>
-    </li>
-  <?php endforeach; ?>
-  </ol>
-</div>
-<section class="padding-y text-white ">
-  <div class="container">
-    <div class="row">
-      <?php foreach($templateParams["catego"] as $category):
-      $parametro =$dbh->getProdottoByCategoria($category["idcategoria"]);
-      foreach($parametro as $prodotto):?>
-	    <div class="col-lg-3 col-md-6 col-sm-6 collapse collapse-horizontal" id="<?php echo $category["nomecategoria"]?>">
-			  <figure class="card card-product-grid bg-dark rounded-6 shadow">
-				    <div class="img-wrap"> 
-              <img src="img/<?php
-                $imgNoSpace = str_replace(' ', '', $prodotto["img"]);            //da togliere e sistemare l'immagine in php
-                 echo $imgNoSpace?>.png" class="figure img-fluid" alt="..."/>
-				    </div>
-				      <figcaption class="info-wrap">
-               <p class="title mb-2"><?php echo $prodotto["nome"]?></p>
-					      <div class="price-wrap">
-						      <span class="price text-end">Prezzo: <?php echo $prodotto["prezzo"] ?>€</span>
-				    	  </div> 
-                <p class="mb-2"><?php echo $prodotto["descrizione"]?></p>
-					      <a href="#" class="btn btn-warning">Add to cart</a>
-					      <a href="#" class="btn btn-light btn-icon text-end"><i class="fa fa-heart"></i> </a>
-				      </figcaption>
-			    </figure> 
-      </div> 
-    <?php endforeach; ?>
-    <?php endforeach; */?>
-  </div>
-</section> -->
 
 <div class="container-lg">
   <ol class="list-group">
@@ -49,7 +8,7 @@
       <div class="ms-2 me-auto" data-bs-toggle="collapse" data-bs-target="#<?php echo $category["nomecategoria"]?>" aria-expanded="false" aria-controls="collapseWidthExample">
         <div class="fw-bold display-1"><?php echo $category["nomecategoria"]?></div>
       </div>
-      <span class="badge"><i class='<?php echo $category["icona"] ?>' style='font-size:48px;color:rgb(247,193,68)'></i></span>
+      <span class="badge"><i class='<?php echo $category["icona"]?>' style='font-size:48px;color:rgb(247,193,68)'></i></span>
     </li>
     <?php endforeach ; ?>
   </ol>
@@ -62,12 +21,12 @@
       foreach($parametro as $prodotto):?>
       <div class="col-lg-3 col-md-6 col-sm-6 collapse collapse-horizontal" id="<?php echo $category["nomecategoria"]?>">
         <figure class="card card-product-grid bg-dark rounded-6 shadow">
-          <div class="img-wrap">
+          <div class="img-wrap text-center">
           <img src="img/<?php
                 $imgNoSpace = str_replace(' ', '', $prodotto["img"]);            //da togliere e sistemare l'immagine in php
-                 echo $imgNoSpace?>.png" class="figure img-fluid" alt="..."/>
+                 echo $imgNoSpace?>.png" class="figure img-fluid  p-3" alt="..." style="height: 220px;"/>
           </div>
-          <figcaption class="info-wrap">
+          <figcaption class="info-wrap px-3">
           <p class="title mb-2"><?php echo $prodotto["nome"]?></p>
 					      <div class="price-wrap">
 						      <span class="price text-end">Prezzo: <?php echo $prodotto["prezzo"] ?>€</span>
@@ -78,8 +37,9 @@
           </figcaption>
         </figure>
       </div>
+      <?php endforeach; ?>
+    <?php endforeach; ?>
     </div>
-    <?php endforeach; ?>
-    <?php endforeach; ?>
+    
   </div>
 </section>
