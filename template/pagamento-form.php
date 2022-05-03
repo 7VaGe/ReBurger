9957b943f0b7974b99bf2975fce3255868da6551
giovanni12perini <99191725+giovanni12perini@users.noreply.github.com>
2022-05-03 17:07:08 +0200

    Update pagamento-form.php

--- template/pagamento-form.php
+++ template/pagamento-form.php
@@ -1,9 +1,9 @@
 <div class="container-lg my-5">
   <div class="row mx-4 p-4 pb-auto pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg bg-white">
-    <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+    <div class="col-lg-7-centered p-3 p-lg-5 pt-lg-3">
       <h1 class="display-4 fw-bold lh-1">Dettagli carta</h1>
       <div class="card-details">
-            <div class="row centered">
+            <div class="row">
               <div class="form-group col-sm-7">
                 <label for="card-holder">Titolare della carta</label>
                 <input id="card-holder" type="text" class="form-control" placeholder="Titolare della carta" aria-label="Titolare della carta" aria-describedby="basic-addon1">
