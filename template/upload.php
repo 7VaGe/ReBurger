974d5f55e08ce65100aae39ff6c3509a89cfd93f
Nicholas Ciarafoni <78082910+7VaGe@users.noreply.github.com>
2022-05-14 23:05:05 +0200

    Bugfix upload.php bootstrap restyling
    eseguito la modifica dello style tramite bootstrap

--- template/upload.php
+++ template/upload.php
@@ -1,33 +1,43 @@
-<div class="text-center form-signin">
+<div class="text-center">
   <form action="profile.php" method="post">
    <?php foreach($templateParams["utente"] as $userInfo):     ?>
-    <h1 class=" my-4 h3 mb-3 text-center text-white fw-normal">Benvenuto, ora puoi modificare il tuo profilo <?php echo strtolower($userInfo["username"]); ?> : </h1>
     <div class="container-lg">
-      <div class="form-floating d-flex">
+    
+    <h1 class=" my-4 h3 mb-3 text-center text-white fw-normal">Benvenuto, ora puoi modificare il tuo profilo <?php echo strtolower($userInfo["username"]); ?> : </h1>
+    <div class="card bg-dark rounded-6 shadow-lg" >
+        <img class="rounded-circle mx-auto d-block my-2 img-fluid" src='img/<?php echo $userInfo["img"]?>' style="width: 200px; height:200px;"/>
+        <div class="container col-lg-6 col-lg-offset-2">
+             <div class="w-100 form-floating d-flex  mt-4">
           <input type="text" class="form-control my-2" id="username" value="<?php echo $userInfo["username"] ?>" name="username" /> <!-- oninvalid="this.setCustomValidity('cambia messaggio')" puoi cambiare il messaggio d'errore-->
           <label for="floatingInput">Nome utente</label>
       </div>
-      <div class="form-floating d-flex">
+      <div class="w-100 form-floating d-flex ">
           <input type="email" class="form-control my-2" id="email" value="<?php echo $userInfo["email"] ?>" name="email" /> <!-- oninvalid="this.setCustomValidity('cambia messaggio')" puoi cambiare il messaggio d'errore-->
           <label for="floatingInput">Email</label>
       </div>
-      <div class="form-floating d-flex">
+      <div class="w-100 form-floating d-flex ">
           <input type="password" class="form-control my-2" id="password" value="<?php echo $userInfo["password"] ?>" name="password" /> <!-- oninvalid="this.setCustomValidity('cambia messaggio')" puoi cambiare il messaggio d'errore-->
           <label for="floatingInput">Password</label>
       </div>
-      <div class="form-floating">
-          <input type="text" class="form-control my-2" id="checkPwd" name="checkPwd" /> <!-- oninvalid="this.setCustomValidity('cambia messaggio')" puoi cambiare il messaggio d'errore-->
+      <div class="w-100 form-floating ">
+          <input type="password" class="form-control my-2" id="checkPwd" name="checkPwd" /> <!-- oninvalid="this.setCustomValidity('cambia messaggio')" puoi cambiare il messaggio d'errore-->
           <label for="floatingInput">Conferma Password</label>
           <button id="toggle-password" type="button" class="btn-sm btn-warning mx-4 d-none b-inline-block"><i id="eyeIcon" class="bi bi-eye-slash"></i></button>
       </div>
       
-      </div>
      <!-- <div class="form-floating d-flex">
           <input type="file" class="form-control my-2" id="img" accept="immmagini/*" value=" echo $userInfo["img"] ?>" name="img" />
           <label for="floatingInput">Immagine utente</label>
       </div>-->
+    
+    
 <?php endforeach;?>
-    <button class="w-100 btn btn-lg btn-warning" type="submit"> Conferma modifiche <i class="fa-solid fa-circle-check"></i></button>
+ <button class="w-50 btn btn-lg btn-warning m-4"  type="submit"> Conferma modifiche  <br> <i class="fa-solid fa-circle-check"></i></button>
+       </div> 
+     
+   </div>
   </form>
+</div>
+  <button class="w-75 btn btn-lg btn-warning my-4"  onclick='windows.location="login.php"'> <i class="fa-solid fa-circle-arrow-left"></i> Torna al tuo profilo</button>
 </div>
   <script src="script/showpwd.js"></script>
