ed7928ea601d32cdaded6bcc46c562370bb6df65
Nicholas Ciarafoni <78082910+7VaGe@users.noreply.github.com>
2022-05-20 13:38:31 +0200

    NewStyle contatti.php
    Modifiche al db, inserito in db util tutte le descrizioni con codice interno

--- template/contatti-form.php
+++ template/contatti-form.php
@@ -3,30 +3,24 @@
     $_GET["indirizzo"]=NULL; $_GET["descrizione"]=NULL; $_GET["nome_locale"]=NULL;
     $templateParams["info"] = $dbh->getInfoVenditore();
   }?>
-  <div class="container-lg mt-2">
+  <div class="container-lg mt-2" id="card">
     <?php foreach ($templateParams["info"] as $info) :?>
-      <div class="row row-cols-1 align-items-stretch g-4"> <!-- ho tolto row-cols-lg-3 che mi dava la forma a quadretto per la card.-->
+      <div class="row row-cols-1 align-items-stretch g-4"> <!-- ho tolto row-cols-lg-3 che mi dava la forma a quadretto per la card.<img class="img img-fluid" src="img/ echo $info["img"]?>" style="height:100%; width:100%;"></img>-->
         <div class="card card-cover  text-white bg-dark rounded-5 shadow-lg">
           <div class="d-flex flex-column text-center pb-3  text-white">
-            <h2 class="pt-5 mb-4 display-1 text-center lh-1 overflow-hidden "><img class="img img-fluid" src="img/<?php echo $info["img"]?>" style="height:100%; width:100%;"></img></h2>
-            
-            <div class="d-flex flex-column display-5 fw-bold text-white text-shadow-1" >
-              <p><?php echo $info["nome"] ?></p>
+            <h2 class="pt-5 mb-4 display-1 text-center lh-1 overflow-hidden "></h2>       
+            <div class="d-flex flex-column display-4 fw-bold text-white text-shadow-1" >
+              <p><img src="img/Logo2.png" alt=""/></p>
             </div>
             <div class="text-white">
-             <p> <?php echo $info["descrizione"] ?></p>
-              
-              <h2><?php echo $info["indirizzo"] ?></h2>
+             <p class="display-6"> <?php echo $info["descrizione"] ?></p>             
             </div>
           </div>
         </div>
       </div>
       <?php endforeach; ?>
-      
   </div>
-
-
-  <div class="container col-xxl-8 px-4 py-5 text-white">
+  <div class="container col-xxl-8 px-4 py-5 text-white fade" id="hero1">
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div class="col-10 col-sm-8 col-lg-6">
         <img src="img/<?php echo $info["img"]?>" class="d-block mx-lg-auto img-fluid" alt="" width="700" height="500" loading="lazy">
@@ -41,8 +35,7 @@
       </div>
     </div>
   </div>
-
-  <div class="container col-xxl-8 px-4 text-white">
+  <div class="container col-xxl-8 px-4 text-white fade"id="hero2">
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div class="col-lg-6">
         <h1 class="display-6 fw-bold lh-1 mb-3">Ricerca incondizionata</h1>
@@ -53,24 +46,20 @@
       </div>
     </div>
   </div>
-  <div class="px-4 text-center text-white">
+  <div class="px-4 text-center text-white fade"id="hero3">
     <h1 class="display-5 fw-bold">Sinceramente parlando</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4"><?php echo $info["descrizione3"];?></p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-        <button type="button" class="btn btn-warning btn-lg px-4 me-sm-3">Primary button</button>
-        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
-      </div>
     </div>
     <div class="overflow-hidden" style="max-height: 30vh;">
       <div class="container px-5">
-        <img src="img/<?php echo $info["img"]?>" class="img-fluid  rounded-3 shadow-lg mb-4" alt="" width="700" height="500" loading="lazy">
-      </div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2863.421765648172!2d12.24379561513581!3d44.1365477290594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132ca4c84ba16541%3A0x6ebdadf647fd2af5!2sVia%20Martiri%20D&#39;Ungheria%2C%206%2C%2047521%20Cesena%20FC!5e0!3m2!1sit!2sit!4v1653045886880!5m2!1sit!2sit" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>      </div>
     </div>
   </div>
-
   <div class="container-lg text-center">
 <?php if(isset($_SESSION["username"]) && $_SESSION["username"]=="propriolui"){ //da sostituire con il controllo se l'utente è o meno il venditore?>
         <button type="button" class="w-75 btn btn-warning mt-4" onclick='window.location="modificaVenditore.php"' >Modifica <i class="fa-solid fa-pen-to-square"></i></button>
       <?php  }?>
 </div>
+
+<script src="script/showhero.js"> </script>
