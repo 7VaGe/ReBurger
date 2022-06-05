21d36ff58186e96f6058ea230eb66eb8decbdbf0
giovanni12perini <99191725+giovanni12perini@users.noreply.github.com>
2022-06-05 14:53:48 +0200

    Sistemazione carrello per mobile

--- template/carrello-form.php
+++ template/carrello-form.php
@@ -30,11 +30,10 @@ input[type='radio']:after {
 
 <div class="container-lg mt-2">
 <div class="card bg-dark rounded-5 shadow-lg">
-<div class="" id="cart" tabindex="-1" role="dialog">
   <div class="container-lg my-5">
-    <div class="row mx-4 p-4 pb-auto pe-lg-0 pt-lg-5 align-items-center text-white">
-        <h5 class="title display-4 fw-bold" id="cartLabel">Carrello</h5>
-      <div class="body">
+    <div class="align-items-center text-white">
+        <h5 class="title display-4 fw-bold">Carrello</h5>
+
         <table class="table table-dark table-hover table-striped border">
           <?php foreach ($templateParams["carrello"] as $prodotto):?>
           <tr id="<?php echo "riga".$prodotto["nome"]; ?>">
@@ -50,7 +49,7 @@ input[type='radio']:after {
         <input class="m-2" type="radio" id="pagamento1" name="pagamento" value="1" checked>
         <label class="m-2 display-6 fw-bold" for="pagamento1">Con carta <i class="fa-solid fa-credit-card"style="color:rgb(247,193,68)"></i></label>
         <br><input class="m-2" type="radio" id="pagamento2" name="pagamento" value="0">
-        <label class="m-2 display-6 fw-bold" for="pagamento2">Alla consegna <i class="fa-solid fa-money-bill-wave" style="color:rgb(247,193,68)"></i></label>
+        <label class="m-2 display-6 fw-bold" for="pagamento2">Contanti<i class="fa-solid fa-money-bill-wave" style="color:rgb(247,193,68)"></i></label>
 
         <div id="spesa">Prezzo totale: € <?php if ($templateParams["conto"]==NULL){
           echo "0";
@@ -71,7 +70,6 @@ input[type='radio']:after {
       </form>
     </div>
   </div>
-</div>
 </div>
 </div>
 
