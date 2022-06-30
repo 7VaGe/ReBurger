562879cf9203e3bf0f06878637a53ddcc693b836
giovanni12perini <99191725+giovanni12perini@users.noreply.github.com>
2022-06-30 19:31:57 +0200

    bugfix orario

--- template/dettagli-form.php
+++ template/dettagli-form.php
@@ -5,7 +5,13 @@
       <h3 class=" my-4 h3 mb-3 text-white fw-normal"><?php echo $ordine["quantita"] . " ". $ordine["nome"];?></h3>
     <?php endforeach;?>
     <?php if ($templateParams["stato"]["stato"]==1){
-      ?><button class="w-100 btn btn-lg btn-warning" onclick="window.location='ordini.php?ordine=<?php echo $ordine['idordine']?>&scelta=accettato'">Accetta ordine</button><?php
+      ?><form class="" action="ordini.php" method="get">
+          <label for="ora" class="text-white">Seleziona un'ora di arrivo</label>
+          <input id="ora" name="ora" type="time" ></input>
+          <input id="ordine" name="ordine" type="number" value="<?php echo $ordine["idordine"]?>" hidden></input>
+          <input id="scelta" name="scelta" type="text" value="accettato" hidden></input>
+          <button class="w-100 btn btn-lg btn-warning mt-3" type="submit">Accetta ordine</button>
+      </form><?php
     }elseif ($templateParams["stato"]["stato"]==2) {
       ?><form class="" action="ordini.php" method="get">
           <label for="ora" class="text-white">Seleziona un'ora di arrivo</label>
