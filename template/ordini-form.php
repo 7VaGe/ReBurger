6132dc07ab51fa0b095b98f2a2ea4baf27dabe54
giovanni12perini <99191725+giovanni12perini@users.noreply.github.com>
2022-06-30 18:09:27 +0200

    piu colori

--- template/ordini-form.php
+++ template/ordini-form.php
@@ -3,31 +3,40 @@
       <div class="row col row-cols-1 row-cols-md-1 g-4">
         <div class="card bg-dark rounded-6 shadow">
               <div class="card-body my-3">
-                  <h5 class="card-title text-white text-center">Ciao <?php echo strtoupper($_SESSION["username"]); ?>, qui puoi visualizzare lo storico degli ordini ricevuti</h5>       
+                  <h5 class="card-title text-white text-center">Ciao <?php echo strtoupper($_SESSION["username"]); ?>, qui puoi visualizzare lo storico degli ordini ricevuti</h5>
                               <!-- tolto un container -->
                                 <?php if( $templateParams["ordine"]== null):?>
                                   <div class="text-white text-center">
                                     <p class="title display-4 fw-bold text-center">Non sono presenti ordini al momento</p>
                                     <img src="img\Carrellovuoto.png" alt="Non ci sono ordini" style="width:350px "/>
-                                </div>                 
+                                </div>
                             </div>
                     <?php else: ?>
                     <div class="accordion" id="accordionExample">
-                    <?php foreach($templateParams["ordine"] as $ordine) :?> 
-                      
+                    <?php foreach($templateParams["ordine"] as $ordine) :?>
+
                       <div class="accordion-item bg-dark">
                         <h2 class="accordion-header" id="heading<?php echo $ordine["idordine"];?>">
-                       
+
                         <!-- Divido qui i vari ordini per stato --->
-                        <?php if($ordine["stato"] != 1 && $ordine["stato"] != 2 && $ordine["stato"] !=3):?>
-                          <button class="accordion-button collapsed bg-dark text-white border-0 rounded-3" id="btnOrd" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $ordine["idordine"];?>" aria-expanded="true" aria-controls="collapse<?php echo $ordine["idordine"];?>">
-                            Ordine #<?php echo $ordine["idordine"];?>
-                            </button>
-                            <?php else:?>
-                              <button class="accordion-button collapsed bg-warning text-white border-0 rounded-3" id="btnOrd" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $ordine["idordine"];?>" aria-expanded="true" aria-controls="collapse<?php echo $ordine["idordine"];?>">
-                            Ordine #<?php echo $ordine["idordine"];?>
-                            </button>
-                            <?php endif;?>
+                        <button class="accordion-button collapsed text-white border-0 rounded-3" id="btnOrd" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $ordine["idordine"];?>" aria-expanded="true" aria-controls="collapse<?php echo $ordine["idordine"];?>"
+                        <?php switch ($ordine["stato"]){
+                          case '3':
+                            echo ' style="background-color:rgb(255,194,0);"';
+                            break;
+
+                          case '5':
+                            echo ' style="background-color:red;"';
+                            break;
+
+                          case '4':
+                            echo ' style="background-color:green;"';
+                            break;
+
+                          default:
+                            echo ' style="background-color:rgb(20,20,41);"';
+                            break;
+                        } ?>>Ordine #<?php echo $ordine["idordine"];?></button>
                           </h2>
                           <div id="collapse<?php echo $ordine["idordine"];?>" class="accordion-collapse collapse " aria-labelledby="heading<?php echo $ordine["idordine"];?>" data-bs-parent="#accordionExample">
                             <div class="accordion-body text-white text-center p-0 pt-3 ">
@@ -52,7 +61,7 @@
             ?>
             <button class="btn-warning btn mb-3" onclick="window.location='dettagliOrdine.php?ordine=<?php echo $ordine['idordine'] ?>'">Accetta ordine</button><?php
             } elseif($ordine["stato"]==2) {
-              ?><button class="btn-warning btn mb-3" onclick="window.location='dettagliOrdine.php?ordine=<?php echo $ordine['idordine'] ?>'">Spedisci ordine</button><?php 
+              ?><button class="btn-warning btn mb-3" onclick="window.location='dettagliOrdine.php?ordine=<?php echo $ordine['idordine'] ?>'">Spedisci ordine</button><?php
           }elseif($ordine["stato"]==3) {?>
           <button class="btn-warning btn mb-3" onclick="window.location='dettagliOrdine.php?ordine=<?php echo $ordine['idordine'] ?>'">Consegnato</button><?php
           }else {
@@ -70,15 +79,14 @@
                     </div>
                   </div>
                 </div>
-              </div> 
-              
+              </div>
+
               <?php endforeach; ?>
           </div>
-          
+
          </div>
         <?php endif; ?>
         </div>
       </div>
     </div>
 </div>
-     
