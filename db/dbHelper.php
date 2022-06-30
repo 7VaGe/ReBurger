565c0e639ac09cbe2baea49c4ff78597d8f50b24
giovanni12perini <99191725+giovanni12perini@users.noreply.github.com>
2022-06-30 23:05:52 +0200

    categorizazione dbhelper

--- db/dbHelper.php
+++ db/dbHelper.php
@@ -11,7 +11,7 @@ class DatabaseHelper{
             die("Connection failed: " . $db->connect_error);
         }
     }
-
+//Funzioni per tabella utente
     public function deleteUtente($idutente, $username){
         $query = "DELETE FROM utente WHERE idutente = ? AND username = ?";
         $stmt = $this->db->prepare($query);
@@ -28,6 +28,75 @@ class DatabaseHelper{
         return $stmt->insert_id;
     }
 
+    public function updateUtente($nome, $email, $password, $id){
+        $query = "UPDATE utente SET username=?, email=?, password=? WHERE idutente=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('sssi',$nome, $email, $password, $id);
+        $stmt->execute();
+      return true;
+    }
+
+    public function checkNomeUtente($username){
+        $query = "SELECT idutente FROM utente WHERE username = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s',$username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function checkEmailUtente($mail){
+        $query = "SELECT idutente FROM utente WHERE email = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s',$mail);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getEmail($id){
+        $stmt = $this->db->prepare("SELECT email FROM utente WHERE idutente=?");
+        $stmt->execute();
+        $stmt->bind_param('s', $id);
+        $result = $stmt->get_result();
+
+        return $result;
+    }
+
+    public function getUtenteById($idcliente){
+        $query = "SELECT * FROM utente WHERE idutente=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idcliente);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+      return $result->fetch_assoc();
+  }
+
+    public function getUtenti(){
+        $query = "SELECT * FROM utente ";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+      return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+//funzione di controllo login
+    public function checkLogin($username, $password){
+        $query = "SELECT idutente, username FROM utente WHERE username = ? AND password = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ss',$username, $password);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+//Funzioni per tabella prodotto
+
     public function insertProdotto($venditore, $nome, $descrizione, $categoria, $prezzo){
         $query = "INSERT INTO prodotto (venditore, nome, descrizione, categoria, prezzo) VALUES (?, ?, ?, ?, ?)";
         $stmt = $this->db->prepare($query);
@@ -37,18 +106,10 @@ class DatabaseHelper{
         return $stmt->insert_id;
     }
 
-    public function insertNews($titolo, $desc){
-        $query = "INSERT INTO notizie (titolo, contenuto) VALUES (?, ?)";
+    public function deleteProdotto($idprodotto){
+        $query = "DELETE FROM prodotto WHERE idprodotto=?";
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param('ss',$titolo, $desc);
-        $stmt->execute();
-        return $stmt->insert_id;
-    }
-
-    public function insertCategoria($nome){
-        $query = "INSERT INTO categoria (nomecategoria) VALUES (?)";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('s', $nome);
+        $stmt->bind_param('i',$idprodotto);
         $stmt->execute();
         return true;
     }
@@ -80,10 +141,6 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-//  Useremo questa funzione per prendere le categorie da far visualizzare e i relativi prodotti, devo capire se
-//  la query restituisce i risultati in un array oppure usa altre strutture, così posso capire dove accedere per prendere
-//  i dati che ci servono.
-
     public function getProdottoById($idprodotto){
         $query = "SELECT * FROM prodotto WHERE idprodotto=?";
         $stmt = $this->db->prepare($query);
@@ -94,88 +151,31 @@ class DatabaseHelper{
       return $result->fetch_assoc();
     }
 
-    public function updateUtente($nome, $email, $password, $id){
-        $query = "UPDATE utente SET username=?, email=?, password=? WHERE idutente=?";
+    public function getPrezzoProdotto($prodotto){
+        $query = "SELECT prezzo FROM prodotto WHERE nome=?";
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param('sssi',$nome, $email, $password, $id);
-        $stmt->execute();
-      return true;
-    }
-
-    public function uploadImmagine($id, $provenienza){
-      $director = "img/";
-      $percorso = $director . basename($_FILES["immagine"]["name"]);
-      $valida = 1;
-      $estensione = strtolower(pathinfo($percorso,PATHINFO_EXTENSION));
-      $img = basename($_FILES["immagine"]["name"]);
-
-      if (file_exists($percorso)) {
-        $valida = 0;
-      }
-
-      if($estensione != "jpg" && $estensione != "png" && $estensione != "jpeg") {
-        $valida = 0;
-      }
-
-      if ($valida == 0) {
-
-        return "Non siamo riusciti ad inserire l'immagine nel nostro database controlli se il file sia della giusta estensione oppure ne modifichi il nome";
-      } else {
-        if ($provenienza=="prodotto" && move_uploaded_file($_FILES["immagine"]["tmp_name"], $percorso)) {
-          $identificatore = $id;
-          $query = "UPDATE prodotto SET img=? WHERE idprodotto=?";
-          $stmt = $this->db->prepare($query);
-          $stmt->bind_param('si', $img, $identificatore);
-          $stmt->execute();
-          return true;
-        }
-        if ($provenienza=="utente" && move_uploaded_file($_FILES["immagine"]["tmp_name"], $percorso)) {
-          $identificatore = $id;
-          $query = "UPDATE utente SET img=? WHERE idutente=?";
-          $stmt = $this->db->prepare($query);
-          $stmt->bind_param('si', $img, $identificatore);
-          $stmt->execute();
-          return true;
-        }
-        if ($provenienza=="notizie" && move_uploaded_file($_FILES["immagine"]["tmp_name"], $percorso)) {
-          $identificatore = $id;
-          $query = "UPDATE notizie SET immagine=? WHERE idnews=?";
-          $stmt = $this->db->prepare($query);
-          $stmt->bind_param('si', $img, $identificatore);
-          $stmt->execute();
-          return true;
-        }
-       }
-     }
-
-    public function checkLogin($username, $password){
-        $query = "SELECT idutente, username FROM utente WHERE username = ? AND password = ?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('ss',$username, $password);
+        $stmt->bind_param('s',$prodotto);
         $stmt->execute();
         $result = $stmt->get_result();
 
-        return $result->fetch_all(MYSQLI_ASSOC);
+        return $result->fetch_assoc();
     }
 
-    public function checkCarrello($nome, $ordine){
-        $query = "SELECT idcarrello FROM carrello WHERE nome = ? AND idordine = ?";
+    public function updateProdotto($nome, $descrizione, $categoria, $prezzo, $id){
+        $query = "UPDATE prodotto SET nome=?, descrizione=?, categoria=?, prezzo=? WHERE idprodotto=?";
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param('si',$nome, $ordine);
+        $stmt->bind_param('ssiii',$nome, $descrizione, $categoria, $prezzo, $id);
+        $stmt->execute();
+        return true;
+    }
+
+    public function getAllProdotti(){
+        $query = "SELECT * FROM prodotto";
+        $stmt = $this->db->prepare($query);
         $stmt->execute();
         $result = $stmt->get_result();
 
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
-
-    public function checkNomeUtente($username){
-        $query = "SELECT idutente FROM utente WHERE username = ?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('s',$username);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
+      return $result->fetch_all(MYSQLI_ASSOC);
     }
 
     public function checkNomeProdotto($nome){
@@ -188,10 +188,60 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function checkEmailUtente($mail){
-        $query = "SELECT idutente FROM utente WHERE email = ?";
+//Funzione per inserimento immagine
+
+    public function uploadImmagine($id, $provenienza){
+        $director = "img/";
+        $percorso = $director . basename($_FILES["immagine"]["name"]);
+        $valida = 1;
+        $estensione = strtolower(pathinfo($percorso,PATHINFO_EXTENSION));
+        $img = basename($_FILES["immagine"]["name"]);
+
+        if (file_exists($percorso)) {
+          $valida = 0;
+        }
+
+        if($estensione != "jpg" && $estensione != "png" && $estensione != "jpeg") {
+        $valida = 0;
+        }
+
+        if ($valida == 0) {
+
+          return "Non siamo riusciti ad inserire l'immagine nel nostro database controlli se il file sia della giusta estensione oppure ne modifichi il nome";
+        } else {
+          if ($provenienza=="prodotto" && move_uploaded_file($_FILES["immagine"]["tmp_name"], $percorso)) {
+            $identificatore = $id;
+            $query = "UPDATE prodotto SET img=? WHERE idprodotto=?";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param('si', $img, $identificatore);
+            $stmt->execute();
+            return true;
+          }
+          if ($provenienza=="utente" && move_uploaded_file($_FILES["immagine"]["tmp_name"], $percorso)) {
+            $identificatore = $id;
+            $query = "UPDATE utente SET img=? WHERE idutente=?";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param('si', $img, $identificatore);
+            $stmt->execute();
+            return true;
+          }
+          if ($provenienza=="notizie" && move_uploaded_file($_FILES["immagine"]["tmp_name"], $percorso)) {
+            $identificatore = $id;
+            $query = "UPDATE notizie SET immagine=? WHERE idnews=?";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param('si', $img, $identificatore);
+            $stmt->execute();
+            return true;
+          }
+        }
+     }
+
+//Funzioni su tabella carrello
+
+    public function checkCarrello($nome, $ordine){
+        $query = "SELECT idcarrello FROM carrello WHERE nome = ? AND idordine = ?";
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param('s',$mail);
+        $stmt->bind_param('si',$nome, $ordine);
         $stmt->execute();
         $result = $stmt->get_result();
 
@@ -239,21 +289,7 @@ class DatabaseHelper{
         return true;
     }
 
-    public function deleteProdotto($idprodotto){
-        $query = "DELETE FROM prodotto WHERE idprodotto=?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('i',$idprodotto);
-        $stmt->execute();
-        return true;
-    }
-    public function deleteNews($idnews){
-        $query = "DELETE FROM notizie WHERE idnews=?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('i',$idnews);
-        $stmt->execute();
-        return true;
-    }
-
+//Funzioni sulla tabella ordine
 
     public function insertOrdine($cliente){
         $query = "INSERT INTO ordine (utente) VALUES (?)";
@@ -262,6 +298,36 @@ class DatabaseHelper{
         $stmt->execute();
 
         return $stmt->insert_id;
+    }
+
+    public function setPrezzo($prezzo, $ordine){
+        $query = "UPDATE ordine SET prezzo = ?+prezzo WHERE idordine=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ii',$prezzo, $ordine);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return true;
+    }
+
+    public function setUtenteInOrdine($utente, $ordine){
+        $query = "UPDATE ordine SET utente=? WHERE idordine=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ii',$utente, $ordine);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return true;
+    }
+
+    public function setOraOrdine($ora, $ordine){
+        $query = "UPDATE ordine SET ora_consegna=? WHERE idordine=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('si',$ora, $ordine);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return true;
     }
 
     public function setPagamentoOrdine($pagamento, $ordine){
@@ -293,32 +359,6 @@ class DatabaseHelper{
         $query = "SELECT stato FROM ordine WHERE idordine=?";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param('i', $idordine);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_assoc();
-    }
-
-    public function getCategories(){
-        $stmt = $this->db->prepare("SELECT * FROM categoria");
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
-
-    public function getNews(){
-        $stmt = $this->db->prepare("SELECT * FROM notizie ORDER BY idnews DESC");
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
-    }
-
-    public function getPrezzoProdotto($prodotto){
-        $query = "SELECT prezzo FROM prodotto WHERE nome=?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('s',$prodotto);
         $stmt->execute();
         $result = $stmt->get_result();
 
@@ -362,6 +402,8 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+//Funzioni tabella venditore
+
     public function utenteIsVenditore($idutente){
         $query = "SELECT idvenditore FROM venditore WHERE utente=?";
         $stmt = $this->db->prepare($query);
@@ -389,31 +431,7 @@ class DatabaseHelper{
         return true;
     }
 
-    public function updateProdotto($nome, $descrizione, $categoria, $prezzo, $id){
-        $query = "UPDATE prodotto SET nome=?, descrizione=?, categoria=?, prezzo=? WHERE idprodotto=?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('ssiii',$nome, $descrizione, $categoria, $prezzo, $id);
-        $stmt->execute();
-        return true;
-    }
-
-    public function getAllProdotti(){
-        $query = "SELECT * FROM prodotto";
-        $stmt = $this->db->prepare($query);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-      return $result->fetch_all(MYSQLI_ASSOC);
-    }
-
-    public function getEmail($id){
-        $stmt = $this->db->prepare("SELECT email FROM utente WHERE idutente=?");
-        $stmt->execute();
-        $stmt->bind_param('s', $id);
-        $result = $stmt->get_result();
-
-        return $result;
-    }
+//Funzione di controllo validita carta
 
     public function checkCarta($titolare, $numero, $cvc){
         $query = "SELECT idcarta FROM carte WHERE titolare = ? AND numero = ? AND cvc = ?";
@@ -425,33 +443,48 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function setPrezzo($prezzo, $ordine){
-        $query = "UPDATE ordine SET prezzo = ?+prezzo WHERE idordine=?";
+//Funzioni su tabella categoria
+
+    public function insertCategoria($nome){
+        $query = "INSERT INTO categoria (nomecategoria) VALUES (?)";
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param('ii',$prezzo, $ordine);
+        $stmt->bind_param('s', $nome);
         $stmt->execute();
-        $result = $stmt->get_result();
 
         return true;
     }
 
-    public function setUtenteInOrdine($utente, $ordine){
-        $query = "UPDATE ordine SET utente=? WHERE idordine=?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('ii',$utente, $ordine);
+    public function getCategories(){
+        $stmt = $this->db->prepare("SELECT * FROM categoria");
         $stmt->execute();
         $result = $stmt->get_result();
 
-        return true;
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function setOraOrdine($ora, $ordine){
-        $query = "UPDATE ordine SET ora_consegna=? WHERE idordine=?";
+//Funzioni su tabella notizie
+
+    public function insertNews($titolo, $desc){
+        $query = "INSERT INTO notizie (titolo, contenuto) VALUES (?, ?)";
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param('si',$ora, $ordine);
+        $stmt->bind_param('ss',$titolo, $desc);
+        $stmt->execute();
+        return $stmt->insert_id;
+    }
+
+    public function getNews(){
+        $stmt = $this->db->prepare("SELECT * FROM notizie ORDER BY idnews DESC");
         $stmt->execute();
         $result = $stmt->get_result();
 
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function deleteNews($idnews){
+        $query = "DELETE FROM notizie WHERE idnews=?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idnews);
+        $stmt->execute();
         return true;
     }
   /*  public function getOrdiniByDate($data){
@@ -472,61 +505,6 @@ class DatabaseHelper{
           return $result->fetch_all(MYSQLI_ASSOC);
       }
 */
-    public function getUtenteById($idcliente){
-        $query = "SELECT * FROM utente WHERE idutente=?";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('i',$idcliente);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-      return $result->fetch_assoc();
-  }
-
-        public function ilVisualizzato($idmittente, $iddestinatario,$limit=30){
-            $query ="UPDATE `messaggi` SET `data_lettura`=NOW() WHERE `id_mittente`=? AND `id_destinatario`=? AND `data_lettura` IS NULL";
-            $stmt = $this->db->prepare($query);
-            $stmt->bind_param('iii',$idmittente, $iddestinatario, $limit);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
-        }
-
-        public function getAllMsg($idmittente, $iddestinatario,$limit=30){
-          $query ="SELECT * FROM `messaggi`WHERE `id_mittente` IN (?,?) AND `id_destinatatio` IN (?,?) ORDER BY `data_invio` DESC LIMIT 0, $limit";
-          $stmt = $this->db->prepare($query);
-          $stmt->bind_param('iii',$idmittente, $iddestinatario, $limit);
-          $stmt->execute();
-          $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
-        }
-
-        public function sendMsg($idmittente, $iddestinatario, $msg){
-            return $this->exec(
-                "INSERT INTO `messaggi` (`id_mittente`, `id_destinatario`, `msg`) VALUES (?,?,?)",
-                [$idmittente, $iddestinatario, $msg]
-            );
-        }
-
-       public function getMsgNonLetti($idutente){
-            $query ="SELECT `id_mittente`, COUNT(*) `nonLetti` FROM `messaggi` WHERE `id_destinatario`=? AND `data_lettura` IS NULL GROUP BY `id_mittente`";
-            $stmt = $this->db->prepare($query);
-            $stmt->bind_param('i',$idutente);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-        return $result->fetch_all(MYSQLI_ASSOC);
-       }
-
-       public function getUtenti(){
-        $query = "SELECT * FROM utente ";
-        $stmt = $this->db->prepare($query);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-      return $result->fetch_all(MYSQLI_ASSOC);
-    }
 }
 
 ?>
