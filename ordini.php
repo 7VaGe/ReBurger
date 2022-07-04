aace836b6dc6b0184c43c2ba2e3fcfa377ff4ee5
giovanni12perini <99191725+giovanni12perini@users.noreply.github.com>
2022-07-04 22:55:07 +0200

    sistemazione email

--- ordini.php
+++ ordini.php
@@ -10,21 +10,21 @@ if (isset($_GET["ordine"]) and isset($_GET["scelta"])) {
       $dbh->updateOrdine($_GET["ora"], 2, $_GET["ordine"]);
       $utenteOrdine=$dbh->getClienteByOrdine($_GET["ordine"]);
       $cliente=$dbh->getUtenteById($utenteOrdine["utente"]);
-      //inviaMail($cliente["username"], $cliente["email"], "Conferma ordine", "Il suo ordine è in preparazione, arrivo stimato per le: ".$_GET["ora"]);
+      inviaMail($cliente["username"], $cliente["email"], "Conferma ordine", "Il suo ordine è in preparazione, arrivo stimato per le: ".$_GET["ora"]);
 
       break;
     case "annullato":
       $dbh->updateStatoOrdine(5,$_GET["ordine"]);
       $utenteOrdine=$dbh->getClienteByOrdine($_GET["ordine"]);
       $cliente=$dbh->getUtenteById($utenteOrdine["utente"]);
-      //inviaMail($cliente["username"], $cliente["email"], "Annullamento ordine", "Il suo ordine è stato annullato, le chiediamo di tornare da noi un'altrogiorno. ");
+      inviaMail($cliente["username"], $cliente["email"], "Annullamento ordine", "Il suo ordine è stato annullato, le chiediamo di tornare da noi un'altrogiorno. ");
 
       break;
     case "spedito":
       $dbh->updateOrdine($_GET["ora"], 3, $_GET["ordine"]);
       $utenteOrdine=$dbh->getClienteByOrdine($_GET["ordine"]);
       $cliente=$dbh->getUtenteById($utenteOrdine["utente"]);
-      //inviaMail($cliente["username"], $cliente["email"], "Ordine spedito", "Il suo ordine è stato spedito, arrivo stimato per le: ".$_GET["ora"]);
+      inviaMail($cliente["username"], $cliente["email"], "Ordine spedito", "Il suo ordine è stato spedito, arrivo stimato per le: ".$_GET["ora"]);
 
       break;
     case "consegnato":
